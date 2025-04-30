1b4dc429ece505d586f146c2e713aec1acfbb971
Matvii Khadzhynov <legend9amer@gmail.com>
2025-04-30 23:38:50 +0300

    hotfix #idk15

--- app/modules/user_farm/repository.py
+++ app/modules/user_farm/repository.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, and_, update, delete, func
 from app.models.user_farm import UserFarm
+from app.models.farm_template import FarmTemplate
 from sqlalchemy.orm import selectinload
 
 class UserFarmRepository:
@@ -15,9 +16,10 @@ class UserFarmRepository:
 
     async def create_farm(self, telegram_id: int, farm_id: int) -> UserFarm:
         existing = await self.db.get(UserFarm, (telegram_id, farm_id))
+        template = await self.db.get(FarmTemplate, farm_id)
         if existing:
             raise ValueError("Farm already owned")
-        new_farm = UserFarm(telegram_id=telegram_id, farm_id=farm_id)
+        new_farm = UserFarm(telegram_id=telegram_id, farm_id=farm_id, level = 1, last_collected=func.extract('epoch', func.now()), current_income=template.base_income, current_upgrade_cost=template.base_price*template.price_multiplier)
         self.db.add(new_farm)
         await self.db.commit()
         return new_farm
