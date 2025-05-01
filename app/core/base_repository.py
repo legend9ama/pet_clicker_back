7b7ff8953a05084210775921f808ed6fa3235625
Matvii Khadzhynov <legend9amer@gmail.com>
2025-05-02 01:45:08 +0300

    aaaavvv

--- app/core/base_repository.py
+++ app/core/base_repository.py
@@ -1,5 +1,6 @@
+from abc import ABC
 from sqlalchemy.ext.asyncio import AsyncSession
 
-class BaseRepository:
+class BaseRepository(ABC):
     def __init__(self, db: AsyncSession):
         self._db = db
--- app/core/base_service.py
+++ app/core/base_service.py
@@ -1,3 +1,5 @@
-class BaseService:
+from abc import ABC
+
+class BaseService(ABC):
     def __init__(self, repo):
         self.repo = repo
