1183304b320446716a1421c111824ba7b419195b
Matvii Khadzhynov <legend9amer@gmail.com>
2025-05-08 22:58:49 +0300

    fixed clicks

--- app/modules/clicks/service.py
+++ app/modules/clicks/service.py
@@ -19,7 +19,7 @@ class ClickService(BaseService):
     async def is_valid(self, telegram_id: int, amount: int) -> bool:
         clicks = await self.repo.get_clicks(telegram_id)
         time_passed = settings.unixtimestamp-clicks.updated_at
-        if time_passed < 7 and amount <= 300:
+        if time_passed < 5 and amount <= 300:
             return True
         if time_passed < 20:
             return False
